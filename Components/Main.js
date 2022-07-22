36624906d444b7712707adfacd3b8a721f163f76
gorkemdurgun <gorkemdurgunn@gmail.com>
2022-07-22 13:05:28 +0300

    try

---
+++ Components/Main.js
@@ -0,0 +1,18 @@
+import {Text, TouchableOpacity, View} from 'react-native';
+import React, {Component} from 'react';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Main</Text>
+        <TouchableOpacity>
+          <Text>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>ggggggggggggg</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
