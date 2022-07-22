3fc81b6cbf33d4bd7da855b62af2399865454d41
gorkemdurgun <gorkemdurgunn@gmail.com>
2022-07-22 13:20:34 +0300

    try2

--- Components/Main.js
+++ Components/Main.js
@@ -1,18 +1,27 @@
-import {Text, TouchableOpacity, View} from 'react-native';
-import React, {Component} from 'react';
+import {Text, TouchableOpacity, View, StyleSheet, Button} from 'react-native';
+import React, {Component, useState} from 'react';
 
-export default class Main extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Main</Text>
-        <TouchableOpacity>
-          <Text>2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>ggggggggggggg</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+export default function Main() {
+  const [num, setNum] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Main</Text>
+      <TouchableOpacity onPress={()=> setNum(num+1)}>
+        <Text style={styles.text}>{num}</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  text: {
+    color: 'purple',
+    fontSize: 48,
+  },
+});
