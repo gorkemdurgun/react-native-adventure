4c197bfd10c7b7eba9e2bc5f1e888a47651ff080
gorkemdurgun <gorkemdurgunn@gmail.com>
2022-07-24 00:28:21 +0300

    increase method, button

--- Components/Main.js
+++ Components/Main.js
@@ -2,26 +2,33 @@ import {Text, TouchableOpacity, View, StyleSheet, Button} from 'react-native';
 import React, {Component, useState} from 'react';
 
 export default function Main() {
-  const [num, setNum] = useState(0);
+  const [num, setNum] = useState(24);
+
+  const increaseNumber = () => {
+    setNum(num + 1);
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Main</Text>
-      <TouchableOpacity onPress={()=> setNum(num+1)}>
+      <TouchableOpacity onPress={increaseNumber}>
         <Text style={styles.text}>{num}</Text>
       </TouchableOpacity>
+      <Button
+        title="Click"
+        onPress={(event)=> console.log(event)}
+        disabled={false}></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     height: '100%',
     alignItems: 'center',
     justifyContent: 'space-around',
   },
   text: {
-    color: 'purple',
+    color: 'lightgray',
     fontSize: 48,
   },
 });
